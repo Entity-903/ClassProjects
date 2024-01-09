6e4ee4e298147d0963a03b2980144595fa5328c2
Entity-903 <charlesmassie021@gmail.com>
2024-01-09 01:42:20 -0700

    Add project files.

---
+++ WorldlyGreetings/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WorldlyGreetings.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
